e67fc265273f86d24a975954abaf7eeca8e3ed56
Siddharthen Sridhar <sridhar3@uci.edu>
2024-11-14 19:30:08 -0800

    need to correctly parse json data

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import json
+import re
+from bs4 import BeautifulSoup
+
+from collections import defaultdict
+
+# may need to use file system instead of dict if memory error
+ROOT_DIR = "ANALYST"
+
+inverted_index = defaultdict(list)
+
+def tokenize(text):
+    tokens = re.findall(r'\b\w+\b', text.lower())
+    return tokens
+
+def get_html(path):
+    with open(path, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "html.parser")
+        return soup.get_text()
+    
+def get_token_freq(tokens):
+    freq = defaultdict(int)
+    for t in tokens:
+        freq[t] += 1
+    return freq
+
+def inv_index(root):
+    
+    doc_count = 0
+    for s, _, fs in os.walk(root):
+        for f in fs:
+            #print(f)
+
+            doc_path = os.path.join(s, f)
+            doc_id = os.path.relpath(doc_path, root)
+            doc_text = get_html(doc_path)
+            tokens = tokenize(doc_text)
+            tf = get_token_freq(tokens)
+
+            for t, f in tf.items():
+                inverted_index[t].append({"id" : doc_id, "frq" : f})
+            
+            doc_count += 1
+    return doc_count
+
+def make_file(file_name="inv_idx.json"):
+    with open(file_name, "w", encoding="utf-8") as f:
+        json.dump(inverted_index, f, indent=4)
+
+
+if __name__ == "__main__":
+    inv_index(ROOT_DIR)
+    make_file()
+    print(len(inverted_index))
+    print(os.path.getsize("inv_idx.json") / 1024)
